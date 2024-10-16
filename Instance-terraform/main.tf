56cb4cd23cc1ee7be4e2c77669f5c383d16b883d
Spd143spd <65090417+Spd143spd@users.noreply.github.com>
2024-10-16 15:10:36 +0530

    Update main.tf

--- Instance-terraform/main.tf
+++ Instance-terraform/main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "Jenkins-SG" {
 resource "aws_instance" "web" {
   ami                    = "ami-0595d6e81396a9efb"  #change your ami value according to your aws instance
   instance_type          = "t2.large"
-  key_name               = "saurabh-key-aws"
+  key_name               = "hemp.pem"
   vpc_security_group_ids = [aws_security_group.Jenkins-SG.id]
   user_data              = templatefile("./script.sh", {})
 
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
 resource "aws_instance" "web2" {
   ami                    = "ami-0595d6e81396a9efb" #change your ami value according to your aws instance 
   instance_type          = "t2.medium"
-  key_name               = "saurabh-key-aws"
+  key_name               = "hemp.pem"
   vpc_security_group_ids = [aws_security_group.Jenkins-SG.id]
   tags = {
     Name = "Monitoring server"
